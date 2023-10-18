c0cd782db765890265f8d7945dacf5e1c8630e8b
sheuerit <sheuerit@gmail.com>
2023-10-18 13:56:39 +0900

    게시글 작성시 작성자의 정보 저장, 날짜 포맷 변경

--- src/com/koreaIT/example/JAM/dao/ArticleDao.java
+++ src/com/koreaIT/example/JAM/dao/ArticleDao.java
@@ -15,12 +15,13 @@ public class ArticleDao {
 		this.conn = conn;
 	}
 
-	public int doWrite(String title, String body) {
+	public int doWrite(int loginedMemberId, String title, String body) {
 		
 		SecSql sql = new SecSql();
 		sql.append("INSERT INTO article");
 		sql.append("SET regDate = NOW(),");
 		sql.append("updateDate = NOW(),");
+		sql.append("memberId = ?,", loginedMemberId);
 		sql.append("title = ?,", title);
 		sql.append("`body` = ?", body);
 
